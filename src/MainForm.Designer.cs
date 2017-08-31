cdf46abc5bdbc62cdb4fac11834f471e77f98031
Iain Ballard <iain.ballard@gocompare.com>
2017-08-31 15:00:40 +0100

    Basic framework

---
+++ src/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace LittleReviewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.LoadProjectButton = new System.Windows.Forms.Button();
+            this.BrowseForProjectDlog = new System.Windows.Forms.FolderBrowserDialog();
+            this.BranchSelectMenu = new System.Windows.Forms.ComboBox();
+            this.StartReviewButton = new System.Windows.Forms.Button();
+            this.EndReviewButton = new System.Windows.Forms.Button();
+            this.ResetStateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusLabel.Location = new System.Drawing.Point(13, 13);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(445, 52);
+            this.StatusLabel.TabIndex = 0;
+            this.StatusLabel.Text = "Checking for git";
+            // 
+            // LoadProjectButton
+            // 
+            this.LoadProjectButton.Location = new System.Drawing.Point(16, 68);
+            this.LoadProjectButton.Name = "LoadProjectButton";
+            this.LoadProjectButton.Size = new System.Drawing.Size(136, 23);
+            this.LoadProjectButton.TabIndex = 1;
+            this.LoadProjectButton.Text = "&Load Project...";
+            this.LoadProjectButton.UseVisualStyleBackColor = true;
+            this.LoadProjectButton.Click += new System.EventHandler(this.LoadProjectButton_Click);
+            // 
+            // BrowseForProjectDlog
+            // 
+            this.BrowseForProjectDlog.Description = "Please select the folder where the project is checked out";
+            this.BrowseForProjectDlog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.BrowseForProjectDlog.ShowNewFolderButton = false;
+            // 
+            // BranchSelectMenu
+            // 
+            this.BranchSelectMenu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BranchSelectMenu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BranchSelectMenu.Enabled = false;
+            this.BranchSelectMenu.FormattingEnabled = true;
+            this.BranchSelectMenu.Location = new System.Drawing.Point(158, 104);
+            this.BranchSelectMenu.Name = "BranchSelectMenu";
+            this.BranchSelectMenu.Size = new System.Drawing.Size(300, 21);
+            this.BranchSelectMenu.TabIndex = 2;
+            // 
+            // StartReviewButton
+            // 
+            this.StartReviewButton.Enabled = false;
+            this.StartReviewButton.Location = new System.Drawing.Point(16, 103);
+            this.StartReviewButton.Name = "StartReviewButton";
+            this.StartReviewButton.Size = new System.Drawing.Size(136, 23);
+            this.StartReviewButton.TabIndex = 3;
+            this.StartReviewButton.Text = "Start Review";
+            this.StartReviewButton.UseVisualStyleBackColor = true;
+            this.StartReviewButton.Click += new System.EventHandler(this.StartReviewButton_Click);
+            // 
+            // EndReviewButton
+            // 
+            this.EndReviewButton.Enabled = false;
+            this.EndReviewButton.Location = new System.Drawing.Point(16, 132);
+            this.EndReviewButton.Name = "EndReviewButton";
+            this.EndReviewButton.Size = new System.Drawing.Size(136, 23);
+            this.EndReviewButton.TabIndex = 4;
+            this.EndReviewButton.Text = "End Review";
+            this.EndReviewButton.UseVisualStyleBackColor = true;
+            this.EndReviewButton.Click += new System.EventHandler(this.EndReviewButton_Click);
+            // 
+            // ResetStateButton
+            // 
+            this.ResetStateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResetStateButton.Enabled = false;
+            this.ResetStateButton.Location = new System.Drawing.Point(322, 68);
+            this.ResetStateButton.Name = "ResetStateButton";
+            this.ResetStateButton.Size = new System.Drawing.Size(136, 23);
+            this.ResetStateButton.TabIndex = 5;
+            this.ResetStateButton.Text = "Reset Changes";
+            this.ResetStateButton.UseVisualStyleBackColor = true;
+            this.ResetStateButton.Click += new System.EventHandler(this.ResetStateButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 168);
+            this.Controls.Add(this.ResetStateButton);
+            this.Controls.Add(this.EndReviewButton);
+            this.Controls.Add(this.StartReviewButton);
+            this.Controls.Add(this.BranchSelectMenu);
+            this.Controls.Add(this.LoadProjectButton);
+            this.Controls.Add(this.StatusLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(486, 207);
+            this.Name = "MainForm";
+            this.Text = "Review Tool";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Button LoadProjectButton;
+        private System.Windows.Forms.FolderBrowserDialog BrowseForProjectDlog;
+        private System.Windows.Forms.ComboBox BranchSelectMenu;
+        private System.Windows.Forms.Button StartReviewButton;
+        private System.Windows.Forms.Button EndReviewButton;
+        private System.Windows.Forms.Button ResetStateButton;
+    }
+}
+
---
+++ src/MainForm.cs
@@ -0,0 +1,182 @@
+﻿using System.Windows.Forms;
+
+namespace LittleReviewer
+{
+    public partial class MainForm : Form
+    {
+        private const string PickABranchMessage = "- pick a branch -";
+
+        private readonly Git git;
+        private string BaseDirectory;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            var gitPath = Git.FindGitExe();
+
+            if (gitPath == null)
+            {
+                State_CantFindGit();
+                return;
+            }
+
+            git = new Git(gitPath);
+            SetStatus("Using git at " + gitPath + ".");
+        }
+
+        private void SetupProject(string selectedPath)
+        {
+            SetStatus("Working...");
+            var ok = git.TryReadRepo(selectedPath, out BaseDirectory);
+            if (!ok)
+            {
+                State_NotARepo(selectedPath);
+                return;
+            }
+
+
+            var currentBranch = git.GetBranchName(BaseDirectory);
+            if (currentBranch != "master")
+            {
+                State_NotInMaster();
+                return;
+            }
+
+            string mergeName;
+            var inMerge = git.HasMergeInProgress(BaseDirectory, out mergeName);
+            if (inMerge)
+            {
+                State_InMerge(mergeName);
+                return;
+            }
+
+            git.FetchAll(BaseDirectory);
+            var branchesAvailable = git.GetUnmergedBranches(BaseDirectory);
+            if (branchesAvailable.Count < 1)
+            {
+                State_NothingToMerge();
+                return;
+            }
+
+            BranchSelectMenu.Items.Clear();
+            BranchSelectMenu.Items.Add(PickABranchMessage);
+            // ReSharper disable once CoVariantArrayConversion
+            BranchSelectMenu.Items.AddRange(branchesAvailable.ToArray());
+            BranchSelectMenu.SelectedIndex = 0;
+            State_ReadyToReview();
+
+        }
+
+        private void State_ReadyToReview()
+        {
+            DisableControls();
+            LoadProjectButton.Enabled = true;
+            StartReviewButton.Enabled = true;
+            BranchSelectMenu.Enabled = true;
+            SetStatus("Project at " + BaseDirectory +" is ready");
+        }
+
+        private void State_InMerge(string mergeName)
+        {
+            DisableControls();
+            LoadProjectButton.Enabled = true;
+            EndReviewButton.Enabled = true;
+            SetStatus("A review has been started in this project: "+mergeName+"\r\nUse Visual Studio to rebuild before testing.");
+        }
+
+        private void State_NothingToMerge()
+        {
+            DisableControls();
+            LoadProjectButton.Enabled = true;
+            SetStatus("There are no unmerged branches to review.");
+        }
+
+        private void State_CantFindGit()
+        {
+            DisableControls();
+            SetStatus("No 'git.exe' was found. Please check it is installed and on the path");
+        }
+
+        private void State_NotInMaster()
+        {
+            DisableControls();
+            LoadProjectButton.Enabled = true;
+            ResetStateButton.Enabled = true;
+            SetStatus("Project at '" + BaseDirectory + "' is not ready for reviews. Press 'Reset State' to undo changes. Changes will be lost.");
+        }
+
+        private void State_NotARepo(string selectedPath)
+        {
+            DisableControls();
+            LoadProjectButton.Enabled = true;
+            SetStatus("Directory '" + selectedPath + "' is not a git repo.");
+        }
+
+
+
+        private void SetStatus(string msg)
+        {
+            StatusLabel.Text = msg;
+            Refresh();
+        }
+
+
+        private void DisableControls()
+        {
+            LoadProjectButton.Enabled = false;
+            ResetStateButton.Enabled = false;
+            StartReviewButton.Enabled = false;
+            EndReviewButton.Enabled = false;
+            BranchSelectMenu.Enabled = false;
+        }
+
+        private void LoadProjectButton_Click(object sender, System.EventArgs e)
+        {
+            var result = BrowseForProjectDlog.ShowDialog();
+            switch (result)
+            {
+                case DialogResult.OK:
+                case DialogResult.Yes:
+                    SetupProject(BrowseForProjectDlog.SelectedPath);
+                    break;
+            }
+        }
+
+        private void ResetStateButton_Click(object sender, System.EventArgs e)
+        {
+            // reset head, checkout master, fetch, reset to origin
+            DisableControls();
+            SetStatus(git.ResetRepoToOriginMaster(BaseDirectory));
+            SetupProject(BaseDirectory);
+        }
+
+        private void StartReviewButton_Click(object sender, System.EventArgs e)
+        {
+            if (BranchSelectMenu.SelectedIndex < 1)
+            {
+                SetStatus("Pick a branch to review");
+                return;
+            }
+
+            // merge branch into wc
+            DisableControls();
+            SetStatus("Working...");
+            SetStatus(git.MergeBranchIntoWorkingCopy(BaseDirectory, BranchSelectMenu.Text));
+            SetupProject(BaseDirectory);
+        }
+
+        private void EndReviewButton_Click(object sender, System.EventArgs e)
+        {
+            // checkout master, fetch, reset to origin
+            DisableControls();
+            SetStatus("Working...");
+            SetStatus(git.ResetRepoToOriginMaster(BaseDirectory));
+            SetupProject(BaseDirectory);
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
